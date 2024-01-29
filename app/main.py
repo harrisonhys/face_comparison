90eb2223db618175a5be61c15371b045d64ff2b8
harianto16 <masharry.net@gmail.com>
2024-01-29 13:17:42 +0700

    update loggin conf

--- app/main.py
+++ app/main.py
@@ -5,13 +5,6 @@ import logging
 
 app = FastAPI(title='Api Face Recognition', debug=True)
 
-@app.on_event("startup")
-async def startup_event():
-    logger = logging.getLogger("log/uvicorn.access")
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-    logger.addHandler(handler)
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
