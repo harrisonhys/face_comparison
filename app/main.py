479d7c48f86bb33ace54dac2728e961ceb7e0fbe
harianto16 <masharry.net@gmail.com>
2024-01-29 13:09:39 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from typing import Union
 from fastapi import FastAPI, File, UploadFile
 from app.controller.recognition import Recognition
+import logging
 
-app = FastAPI(title='Api Face Recognition')
+app = FastAPI(title='Api Face Recognition', debug=True)
+
+@app.on_event("startup")
+async def startup_event():
+    logger = logging.getLogger("log/uvicorn.access")
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    logger.addHandler(handler)
 
 @app.get("/")
 def read_root():
